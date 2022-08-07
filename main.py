040c4388d30601988a687f7d3529c7839b096cd5
Rahul Beniwal <rahul.b@lendenclub.com>
2022-08-07 17:43:39 +0530

    adding image

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ parser = Parser()
 test = [
     ('1 + 2', 3),
     (' 2 * 2 + 3', 7),
-    (' 10 * ( 5 * 6 ) + 7', 307),
+    (' 10 * ( 5 * 6 ) + 7 ()', 307),
     ('11 - 11 * 20', -209),
     ('(2 - 2 ) * 10', 0)
 ]
@@ -12,6 +12,7 @@ test = [
 result_set = []
 for expr, expected_result in test:
     result = parser.parse(expr)
+    print(f"{expr} => {result}")
     result_set.append(result == expected_result)
 
 if all(result_set):
--- parser.py
+++ parser.py
@@ -1,7 +1,7 @@
 from gc import collect
-import re 
+import re
 import collections
-from typing import OrderedDict 
+from typing import OrderedDict
 
 
 NUM = r'(?P<NUM>\d+)'
@@ -18,32 +18,32 @@ master_pattern = re.compile('|'.join(
 ))
 
 # Tokenizer
-# namedtuple uses less memory than dict 
+# namedtuple uses less memory than dict
 
 Token = collections.namedtuple('Token', ['type', 'value'])
 
 def tokenizer(text):
-    # scanner method repeatdly matches the pattern against 
-    # the regex untill no match is found 
+    # scanner method repeatdly matches the pattern against
+    # the regex untill no match is found
     scanner = master_pattern.scanner(text)
     for m in iter(scanner.match, None):
         tok = Token(m.lastgroup, m.group())
         if tok.type != 'WS':
-            yield tok 
-            
+            yield tok
+
 
 class Parser:
-    
+
     def parse(self, text):
         self.tokens = tokenizer(text)
-        self.token = None 
+        self.token = None
         self.nexttoken = None
         self.order_list = []
         self._advance()
-        return self.expr() 
-    
-    
-    
+        return self.expr()
+
+
+
     def _advance(self):
         tok = next(self.tokens, None)
         '''Advance one token ahead'''
@@ -52,43 +52,43 @@ class Parser:
     def __accept(self, toktype):
         if self.nexttoken and self.nexttoken.type == toktype:
             self._advance()
-            return True 
+            return True
         return False
-    
+
     def _except(self, toktype):
         if not self.__accept(toktype):
             raise SyntaxError('Error -> Expected ' + toktype)
-    
+
     def expr(self):
         exprval = self.term()
         self.order_list.append(exprval)
         while self.__accept('PLUS') or self.__accept('MINUS') \
             or self.__accept('RPAREN'):
-            
-            # token is ) then return the calculated value in a () 
+
+            # token is ) then return the calculated value in a ()
             if self.tok.type == 'RPAREN':
                 return exprval
-            op = self.tok.type 
+            op = self.tok.type
             right = self.term()
             if op == 'PLUS':
                 exprval += right
             elif op == 'MINUS':
                 exprval -= right
-        return exprval 
-    
-    
+        return exprval
+
+
     def term(self):
         termval = self.factor()
         while self.__accept('TIMES') or self.__accept("DIVIDE"):
             op = self.tok.type
-            right = self.factor() # get the operand 
+            right = self.factor() # get the operand
             if op == 'TIMES':
                 termval *= right
             elif op == 'DIVIDE':
                 termval /= right
         return termval
-    
-    
+
+
     def factor(self):
         if self.__accept('NUM'):
             val = self.tok.value
@@ -98,4 +98,4 @@ class Parser:
             return exprval
         else:
             raise SyntaxError('Error -> Expected NUM or LPAREN')
-        
+
